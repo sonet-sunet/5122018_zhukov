cd9a7e216b099c1153de401347542bb15b1d458a
sonet-sunet <slava.zhukov@gmail.com>
2019-04-05 11:24:07 +0300

    Изменен футер

--- parts/footer.php
+++ parts/footer.php
@@ -2,5 +2,6 @@
     <?php foreach( $pageConfig['jsFiles'] as $path_js): ?>
         <script src="<?=$path_js?>"></script>
     <?php endforeach; ?>
+    <div class='footer'>Это наш футер</div>
 </body>
 </html> 
